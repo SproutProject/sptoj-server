33bbf4b67a6b6f8ecf3cdb297bdb333d7640df6d
PZ Read <netfirewall@gmail.com>
2017-03-09 18:37:49 +0000

    Check rejudge permission

--- view/challenge.py
+++ view/challenge.py
@@ -198,6 +198,8 @@ class ListHandler(APIHandler):
 class RejudgeHandler(APIHandler):
     '''Rejudge challenge handler.'''
 
+    level = UserLevel.kernel
+
     async def process(self, data=None):
         '''Process the request.
 
